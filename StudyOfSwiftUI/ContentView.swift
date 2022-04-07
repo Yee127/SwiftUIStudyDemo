473b878112c82260b41a773b741b2cd52ab2a33f
Yee <yeevan@126.com>
2022-04-07 18:33:46 +0800

    Initial Commit

---
+++ StudyOfSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StudyOfSwiftUI
+//
+//  Created by Yee Van on 2022/4/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StudyOfSwiftUI/StudyOfSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyOfSwiftUIApp.swift
+//  StudyOfSwiftUI
+//
+//  Created by Yee Van on 2022/4/7.
+//
+
+import SwiftUI
+
+@main
+struct StudyOfSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
